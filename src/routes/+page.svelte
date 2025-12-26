df923592df46860747b484a3aa2378814d17da75
abhijith-p-subash <abhijith.p.subash@gmail.com>
2025-12-26 20:13:37 +0530

    feat: Replace multiple static SVG assets with a new PNG logo and update `+page.svelte` to use it.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -464,9 +464,9 @@
   >
     <div class="flex items-center space-x-3">
       <div
-        class="w-8 h-8 bg-red-500 rounded-lg flex items-center justify-center shadow-lg shadow-red-500/20"
+        class="w-8 h-8 rounded-full flex items-center justify-center shadow-lg shadow-red-500/20"
       >
-        <svg
+        <!-- <svg
           xmlns="http://www.w3.org/2000/svg"
           width="18"
           height="18"
@@ -481,7 +481,8 @@
           <path
             d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"
           ></path>
-        </svg>
+        </svg> -->
+        <img src="/logo.png" alt="" srcset="" />
       </div>
       <div>
         <h1 class="text-sm font-bold text-white tracking-tight">
